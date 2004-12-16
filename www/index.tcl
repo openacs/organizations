1883bf028cda5c7b6eef773a24888ac8aa7809e7
Jade Rubick <jade@rubick.com>
2004-12-16 00:03:39 +0000

    Fixed edit link to organizations.

--- www/index.tcl
+++ www/index.tcl
@@ -60,13 +60,10 @@ template::list::create \
             label {}
             display_template {
                <if @orgs.write@>
-                <a href="add-edit" title="Edit this organization">
+                <a href="add-edit?organization_id=@orgs.organization_id@" title="Edit this organization">
 		<img src="/shared/images/Edit16.gif" height="16" width="16" 
                 alt="Edit" border="0"></a>
                 </if>
-                <else>
-                @orgs.write@
-                </else>
             }
         }
         name {
