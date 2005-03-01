59fa3d274ba4d815a0d511eade788563ce81fdee
Jeff Davis <davis@xarg.net>
2005-03-01 00:01:40 +0000

    change ad_verify_and_get_user_id to ad_conn user_id, change ad_maybe_redirect_for_registration to auth::require_login, make some db_ and dt_ function -public

--- www/add-edit.tcl
+++ www/add-edit.tcl
@@ -12,7 +12,7 @@ ad_page_contract {
 set package_id [ad_conn package_id]
 
 # The id of the person logged in and browsing this page
-set user_id [ad_maybe_redirect_for_registration]
+set user_id [auth::require_login]
 
 # address of person logged in
 set peeraddr [ad_conn peeraddr]
